642e57ddd5c7ec2714d5a99c3466d08e7d2fa02f
Nathaniel MacArthur-Warner <nathaniel.macarthur-warner@energysage.com>
2023-07-11 12:07:51 -0400

    Implementation of getting tags

--- migrateWPtoStoryblok.js
+++ migrateWPtoStoryblok.js
@@ -4,6 +4,7 @@ import 'dotenv/config'
 import * as fs from 'fs'
 import { fileURLToPath } from 'url'
 import * as path from "path"
+import axios from "axios";
 
 // Load in the slugs of articles we want to migrate.
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
@@ -72,6 +73,21 @@ const handleShortcoderShortcode = async (block) => {
 
 const getRealPath = (data) => {
     return `blog/${data.slug}/?preview=true`
+}
+
+const tagIdToName = new Map()
+
+const getTags = async (data) => {
+    const tagNames = []
+    for (const tagId of data.tags) {
+        if (!tagIdToName.has(tagId)) {
+            const url = `${wp2storyblok.wp.endpoint}/wp/v2/tags/${tagId}/`
+            const req = await axios.get(url)
+            tagIdToName.set(tagId, req.data.name)
+        }
+        tagNames.push(tagIdToName.get(tagId))
+    }
+    return tagNames
 }
 
 const newAuthorSlugToUuid = new Map()
@@ -198,6 +214,7 @@ const wp2storyblok = new Wp2Storyblok(process.env.WP_ENDPOINT, slugs, {
                 ["title", "name"],
                 ["slug", "slug"],
                 [getRealPath, "path"],
+                [getTags, "tag_list"],
                 ["_links.wp:featuredmedia.0", {
                     "field": "content.ArticleImage",
                     "component": "ArticleImage",
