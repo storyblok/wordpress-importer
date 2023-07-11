52b91addd1b94c6ee9bdf23baa63a1c40dd79615
Nathaniel MacArthur-Warner <nathaniel.macarthur-warner@energysage.com>
2023-07-11 15:23:06 -0400

    Add script for fetching all media details

---
+++ fetchMediaJson.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import fs from "fs"
+import "dotenv/config"
+
+const listOfData = []
+
+let page_max_i = 1
+for (let page_i = 1; page_i <= page_max_i; page_i++) {
+    const url = `${process.env.WP_ENDPOINT}/wp/v2/media/?per_page=100&page=${page_i}`
+    const req = await axios.get(url)
+    if (page_i === 1) {
+        page_max_i = req.headers['X-WP-TotalPages'] || req.headers['x-wp-totalpages']
+    }
+    const data = req.data
+    listOfData.push(...data)
+}
+
+const dataAsMapping = listOfData.reduce((result, item) => {
+    result[item.guid.rendered] = item
+    return result
+}, {})
+
+fs.writeFileSync('media.json', JSON.stringify(dataAsMapping, null, 4))
