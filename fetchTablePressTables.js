62c105e4e19a988361b46f961a9b5c34b3bffc5d
Nathaniel MacArthur-Warner <nathaniel.macarthur-warner@energysage.com>
2023-07-24 12:01:43 -0400

    Get to the right page

--- fetchTablePressTables.js
+++ fetchTablePressTables.js
@@ -2,6 +2,8 @@ import puppeteer from 'puppeteer';
 import "dotenv/config"
 
 (async () => {
+    const wp_base_url = process.env.WP_BASE_URL
+
     // Launch the browser and open a new blank page
     const browser = await puppeteer.launch({
         headless: 'new',
@@ -10,13 +12,15 @@ import "dotenv/config"
     await page.setViewport({width: 1080, height: 1024});
 
     // Log in
-    await page.goto(process.env.WP_ADMIN_LOGIN_URL)
+    await page.goto(`${wp_base_url}/admin`)
     await page.type('#user_login', process.env.WP_ADMIN_USERNAME)
     await page.type('#user_pass', process.env.WP_ADMIN_PASSWORD)
     await page.click('#wp-submit')
 
+    await page.goto(`${wp_base_url}/wp-admin/admin.php?page=tablepress_export`)
+
     const textSelector = await page.waitForSelector(
-        'text/Your site has a critical'
+        'text/Exporting a table'
     )
     const fullText = await textSelector?.evaluate(el => el.textContent)
     console.log(fullText)
